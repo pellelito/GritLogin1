1b668f448f3bcdf71106843449b6c02b93916e79
pellelito <pelle.lofstrand@gmail.com>
2020-10-14 18:29:19 +0200

    something changed

--- src/gritLogin/Validate.java
+++ src/gritLogin/Validate.java
@@ -16,22 +16,21 @@ public class Validate {
 	
 	public static boolean valEmail(String user) {
 		
-		boolean valid = false;
+		
 		if (null != user) {         
 			//requires a valid email
 			String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(user);
- 
-            if (matcher.matches()) valid = true;
             
-        }
-									//alternative email validator___________________________
-							        boolean javax = EmailValidator.emailValidator(user);
-							        System.out.println("Javax says validation to be " + javax);
-							        //_________________________________________________________
-							        
-		return valid;
+																            //alternative email validator___________________________
+																	        boolean javax = EmailValidator.emailValidator(user);
+																	        System.out.println("Javax says validation to be " + javax);
+																	        //_________________________________________________________
+																	        
+            if (matcher.matches()) return true;          
+        }								
+		return false;
 	}
 	
 	public static boolean valPassw(String passw) {
@@ -39,7 +38,7 @@ public class Validate {
 		if (null != passw) { 
 			
 		//requires 2 upper case, 2 lower case, 2 numbers, 1 special char and NO spaces
-	     String regex ="^(?=.{7,}$)(?=(?:.*[A-Z]){2})(?=.*[a-z]{2})(?=(?:.*[0-9]){2})(?=.*[@#$%^&+=*©£∞§!€])(?=\\S+$).*";
+	     String regex ="^(?=.{7,}$)(?=(?:.*[A-Z]){2})(?=.*[a-z]{2})(?=(?:.*[0-9]){2})(?=.*[@#$%^&+-=*©£∞§!€])(?=\\S+$).*";
 	     Pattern pattern = Pattern.compile(regex);
          Matcher matcher = pattern.matcher(passw);
          
@@ -61,8 +60,7 @@ public class Validate {
 				String line = br.readLine(); 
 				
 				// loop until all lines are read 
-				while (line != null) { 
-			 
+				while (line != null) {  
 					String[] attributes = line.split("#");
 					line = br.readLine(); 
 					
@@ -71,8 +69,6 @@ public class Validate {
 					}
 			} catch (IOException ioe) { ioe.printStackTrace(); 
 		} 
-		
-		
 		return false;
 	}
 	
